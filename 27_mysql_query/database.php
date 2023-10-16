1d93c910288c564a1703c35a6cebd51200a01ca5
drmagu <willywouters.phd@gmail.com>
2023-10-16 14:39:12 -0500

    27_mysql_query done

--- 27_mysql_query/database.php
+++ 27_mysql_query/database.php
@@ -6,7 +6,6 @@ $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "businessdb";
-$conn = "";
 
 function connect($db_server, $db_user, $db_pass, $db_name){
     try {
--- 27_mysql_query/index.php
+++ 27_mysql_query/index.php
@@ -7,3 +7,38 @@ function br() {
 }
 
 include('./database.php');
+?>
+
+<?php
+// pick one user
+$sql = "SELECT * FROM users WHERE user = 'Patrick'";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    var_dump($row); br();
+} else {
+    echo  'No results from query'; br();
+}
+br();
+?>
+
+<?php
+// pick all users
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row["id"]; br();
+        echo $row["user"]; br();
+        echo $row["reg_date"]; br();
+    }
+} else {
+    echo  'No results from query'; br();
+}
+br();
+?>
+
+<?php
+mysqli_close($conn);
