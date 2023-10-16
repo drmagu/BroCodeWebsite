b24c1e6ba861db345a94161e9efc678332cf3d3b
drmagu <willywouters.phd@gmail.com>
2023-10-15 21:58:40 -0500

    24_connect_mysql finished

---
+++ 24_connect_mysql/index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+function br() {
+    echo '<br />';
+}
+
+include('database.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello</p>
+</body>
+</html>
